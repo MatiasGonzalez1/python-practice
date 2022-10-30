99b6b1571424d679c655f7a5918355992550afb0
Matias Gonzalez <mng_epson@hotmail.com>
2022-10-29 21:59:41 -0300

    feat: init proyecto_personajes

---
+++ entrada_salida/mostrar_datos.py
@@ -0,0 +1,6 @@
+nombre = input('Ingresa tu nombre: ')
+edad = int(input('Ingresa tu edad: '))
+
+print('Hola '+ nombre + ' tienes ' + str(edad)+ ' años')
+print('Hola ', nombre, ' tienes ', edad, ' años' )
+print('Hola {} tienes {}'.format(nombre, edad))
