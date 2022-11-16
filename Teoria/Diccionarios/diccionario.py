62bc7442616da498373b524c8655c50b77998289
Matias Gonzalez <mng_epson@hotmail.com>
2022-11-16 19:23:34 -0300

    feat: add new qr

---
+++ Teoria/Diccionarios/diccionario.py
@@ -0,0 +1,63 @@
+"""Ejercicio 1
+
+En el siguiente diccionario se encuentran capitales de los paises en el mundo, debes realizar un programa que pida un pais al usuario, y muestre la capital de ese pais, en dado caso el pais no este en el diccionario, se debe mostrar un mensaje diciendo que ese pais no se encuentra.
+
+{"Guatemala": "Ciudad de Guatemala", "El Salvador": "San Salvador", "Honduras": "Honduras","Nicaragua": "Managua", "Costa Rica": "San Jose", "Panama": "Panama", "Argentina": "Buenos Aires", "Colombia": "Bogota", "Venezuela": "Caracas", "España": "Madrid"}
+
+
+
+Ejercicio 2
+
+Con el siguiente diccionario, debes crear un programa que pregunte al usuario por un número; el programa debe imprimir el jugador al que hace referencia ese número
+
+{
+
+    1 : "Casillas", 15 : "Ramos",
+
+    3 : "Pique", 5 : "Puyol",
+
+    11 : "Capdevila", 14 : "Xabi Alonso",
+
+    16 : "Busquets", 8 : "Xavi Hernandez",
+
+    18 : "Pedrito", 6 : "Iniesta",
+
+    7 : "Villa"
+
+}"""
+
+
+"""paises = {"Guatemala": "Ciudad de Guatemala", "El Salvador": "San Salvador", "Honduras": "Tegucigalpa","Nicaragua": "Managua", "Costa Rica": "San Jose", "Panama": "Panama", "Argentina": "Buenos Aires", "Colombia": "Bogota", "Venezuela": "Caracas", "España": "Madrid"}
+
+ingreso = input('Ingresa un pais: ')
+letra = ingreso.capitalize() in paises
+
+if letra == True:
+    print(paises[ingreso.capitalize()])
+else:
+    print('No se encuentra el país')
+  """
+  
+  
+jugadores = {
+
+    1 : "Casillas", 15 : "Ramos",
+
+    3 : "Pique", 5 : "Puyol",
+
+    11 : "Capdevila", 14 : "Xabi Alonso",
+
+    16 : "Busquets", 8 : "Xavi Hernandez",
+
+    18 : "Pedrito", 6 : "Iniesta",
+
+    7 : "Villa"
+
+}
+
+ingreso = int(input('Ingrese su número: '))
+
+if ingreso in jugadores:
+    print(jugadores[ingreso])
+else:
+    print('El numero no corresponde a un jugador')
--- generador_qrCode/qr_code.py
+++ generador_qrCode/qr_code.py
@@ -4,7 +4,7 @@ from turtle import back
 import qrcode
 
 #se especifica la información que tendrá el codigo
-website_link = 'https://github.com/MatiasGonzalez1'
+website_link = 'https://galeria-expo-talleres2022-327k5i96k-matiasgonzalez1.vercel.app/'
 #se moldea el tamaño, la cantidad de pixeles y que grosor tendrá
 qr = qrcode.QRCode(version=1, box_size=5, border=5)
 #se agrega el valor al qr
@@ -14,5 +14,5 @@ qr.make()
 
 img = qr.make_image(fill_color = 'black', back_color = 'white')
 
-img.save('github_qr.png')
+img.save('galeria_qr.png')
 
