265b555a0d12a947a11cc3b930ac7c2b0bd21d4d
Matias Gonzalez <mng_epson@hotmail.com>
2022-12-06 13:51:32 -0300

    feat : add teoria/tuplas

---
+++ Teoria/tuplas/tuplas.py
@@ -0,0 +1,25 @@
+# Ejercicio 1
+
+# Escribir una tupla con los meses del año, luego, pide al usuario un numero, el que haya ingresado, es el mes que debe mostrar en la tupla
+
+# meses = ('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre')
+
+# numero = int(input('Ingrese un numero: '))
+# numero -= 1
+
+# print('Tu mes es', meses[numero])
+
+# Ejercicio 2
+
+# Escribir una tupla que tenga las letras del alfabeto. Luego, debes pedir al usuario un número, el que haya ingresado, es la letra que debe imprimir el programa
+
+
+letras = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+
+numero = int(input('Ingrese un numero del 1 al 27: '))
+
+numero -= 1
+
+print('Tu letra es ',letras[numero])
+
+
